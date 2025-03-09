4356f66b31b93960aeb763e5c25022f4128ccaf7
nechamiToledano <nechami3142@gmail.com>
2025-03-09 02:29:30 +0200

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '../components/layout/layout.component';
+import { AuthGuard } from '../guards/auth.guard';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { CoursesComponent } from '../components/courses/courses.component';
+import { CourseManagementComponent } from '../components/course-management/course-management.component';
+import { CourseDetailsComponent } from '../components/course-details/course-details.component';
+import { HomeComponent } from '../components/home/home.component';
+import { LessonsComponent } from '../components/lessons/lessons.component';
+import { AuthComponent } from '../components/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+
+  {
+    path: '',
+    component: LayoutComponent, // ✅ Wrap pages inside LayoutComponent
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'auth', component: AuthComponent },
+      { path: 'courses', component: CoursesComponent, canActivate: [AuthGuard] },
+      { path: 'course-management', component: CourseManagementComponent, canActivate: [AuthGuard] },
+      { path: 'courses/:id', component: CourseDetailsComponent, canActivate: [AuthGuard] },
+      { path: 'lessons/:courseId', component: LessonsComponent },
+
+    ],
+  },
+];
